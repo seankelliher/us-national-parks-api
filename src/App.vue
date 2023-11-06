adb59db7bdc17bb5f76ded48400d9cbced3b8445
Sean Kelliher <sbkelliher@icloud.com>
2023-11-05 19:57:19 -0500

    Remove HeaderPart and FormResults components.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
-import HeaderPart from "./components/HeaderPart.vue";
 import ImageGallery from "./components/ImageGallery.vue";
-//import FormResults from "./components/FormResults.vue";
 </script>
 
 <template>
     <div id="container">
-        <HeaderPart headline="United States National Parks" />
         <ImageGallery />
-        <!--<FormResults />-->
     </div>
 </template>
 
