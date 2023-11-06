0e42b4ccd18d546fef4172018dd3a2af360194c4
Sean Kelliher <sbkelliher@icloud.com>
2023-11-05 19:52:03 -0500

    Add sub-headline. Add 'restoreDefault', click logo to reset gallery.

--- src/components/HeaderPart.vue
+++ src/components/HeaderPart.vue
@@ -3,14 +3,31 @@ defineProps({
     headline: {
         type: String,
         required: true
+    },
+    subHeadline: {
+        type: String,
+        required: true
     }
 });
+
+defineEmits(["restoreDefault"]);
 </script>
 
 
 <template>
     <header>
-        <h1>{{ headline }}</h1>
+        <div class="hline-logo">
+            <figure
+                @click="$emit('restoreDefault')"
+                class="logo"
+            >
+                <img src="images/national-park-service-logo.svg" alt="National Park Service logo">
+            </figure>
+            <h1>{{ headline }}</h1>
+        </div>
+        <div class="sline">
+            <p>{{ subHeadline }}</p>
+        </div>
     </header>
     
 </template>
