f1fa1deb804164c9df37b95d1c04460574e6b56c
Sean Kelliher <sbkelliher@icloud.com>
2023-11-12 14:31:53 -0500

    Add './' to national-park-service-logo.svg URL. Fixing Heroku deployment problems.

--- src/components/HeaderPart.vue
+++ src/components/HeaderPart.vue
@@ -20,7 +20,7 @@ defineEmits(["restoreDefault"]);
             @click="$emit('restoreDefault')"
             class="logo"
         >
-            <img src="images/national-park-service-logo.svg" alt="National Park Service logo">
+            <img src="./images/national-park-service-logo.svg" alt="National Park Service logo">
         </figure>
         <div class="headlines">
             <h1>{{ title }}</h1>
